f189ffae9688f347d7fa6ca90e81c79a21adc200
Ruihang Xia <waynestxia@gmail.com>
2022-07-22 16:29:14 +0800

    uncommont explain
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

--- tests/cases/local/07_optimizer/optimizer.sql
+++ tests/cases/local/07_optimizer/optimizer.sql
@@ -2,8 +2,6 @@ DROP TABLE `07_optimizer_t`;
 
 CREATE TABLE `07_optimizer_t` (name string TAG, value double NOT NULL, t timestamp NOT NULL, TIMESTAMP KEY(t)) ENGINE=Analytic with (enable_ttl='false');
 
--- SHOW CREATE TABLE `07_optimizer_t`;
-
--- EXPLAIN SELECT max(value) AS c1, avg(value) AS c2 FROM `07_optimizer_t` GROUP BY name;
+EXPLAIN SELECT max(value) AS c1, avg(value) AS c2 FROM `07_optimizer_t` GROUP BY name;
 
 DROP TABLE `07_optimizer_t`;
