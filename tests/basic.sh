215dae4d603759ad0d704144fa99a5883876b037
Wei Zhang <kweizh@gmail.com>
2022-07-30 16:46:50 +0800

    build(ci): :hammer: refactor due to review

--- tests/basic.sh
+++ tests/basic.sh
@@ -7,47 +7,23 @@ PORT=${CERESDB_PORT:-"5440"}
 
 URL="http://${ADDR}:${PORT}/sql"
 
-## sleep 10s currently, until we find a better way to check server ready
-sleep 10
-### temporarily disable error exit
-##set -e
-##for i in `seq 0 30`;
-##do
-##    if curl --location ${URL} ; then
-##        echo "server ready, start to test"
-##        break
-##    else
-##        sleep 1
-##    fi
-##done
-##set +e
+function ceresdb_query {
+    sql=${1}
 
-curl --location --request POST ${URL} \
-     --header 'Content-Type: application/json' \
-     --data-raw '{
-        "query": "CREATE TABLE `demo` (`name` string TAG, `value` double NOT NULL, `t` timestamp NOT NULL, TIMESTAMP KEY(t)) ENGINE=Analytic with (enable_ttl='\''false'\'')"
+    curl --location --fail-with-body \
+         --request POST ${URL} \
+         --header 'Content-Type: application/json' \
+         --data-raw '{
+        "query": "'${sql}'"
     }'
+}
 
-curl --location --request POST ${URL} \
-     --header 'Content-Type: application/json' \
-     --data-raw '{
-        "query": "INSERT INTO demo(t, name, value) VALUES(1651737067000, '\''ceresdb'\'', 100)"
-    }'
+ceresdb_query 'CREATE TABLE `demo` (`name` string TAG, `value` double NOT NULL, `t` timestamp NOT NULL, TIMESTAMP KEY(t)) ENGINE=Analytic with (enable_ttl='\''false'\'')'
 
-curl --location --request POST ${URL} \
-     --header 'Content-Type: application/json' \
-     --data-raw '{
-        "query": "select * from demo"
-    }'
+ceresdb_query 'INSERT INTO demo(t, name, value) VALUES(1651737067000, '\''ceresdb'\'', 100)'
 
-curl --location --request POST ${URL} \
-     --header 'Content-Type: application/json' \
-     --data-raw '{
-        "query": "show create table demo"
-    }'
+ceresdb_query 'select * from demo'
 
-curl --location --request POST ${URL} \
-     --header 'Content-Type: application/json' \
-     --data-raw '{
-        "query": "DROP TABLE demo"
-    }'
+ceresdb_query 'show create table demo'
+
+ceresdb_query 'DROP TABLE demo'
