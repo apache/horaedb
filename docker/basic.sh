c2d17ebd48535f0c886589033d553b189eb666c9
Wei Zhang <kweizh@gmail.com>
2022-07-31 20:36:44 +0800

    build(ci): Test docker image build (#150)
    * build(ci): :sparkles: add build image ci on Docker file changes
    
    * build(ci): :sparkles: add basic test and test the docker image
    
    Signed-off-by: WeiZhang <kweizh@gmail.com>
    
    * build(ci): :hammer: also run workflow if github file changed
    
    Signed-off-by: WeiZhang <kweizh@gmail.com>
    
    * build(ci): :hammer: wait until server ready to test
    
    * build(ci): :hammer: sleep 10s directly to wait server ready
    
    * build(ci): :hammer: refactor due to review
    
    * build(ci): :bug: fix unquoted args
    
    * build(ci): :hammer: also test when test file changes
    
    * build(ci): :hammer: move basic.sh under docker, fix -e
    
    * build(ci): :bug: fix changed test file path

---
+++ docker/basic.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -exo pipefail
+
+ADDR=${CERESDB_ADDR:-"127.0.0.1"}
+PORT=${CERESDB_PORT:-"5440"}
+
+URL="http://${ADDR}:${PORT}/sql"
+
+function ceresdb_query {
+    sql=${1}
+
+    curl --location --fail \
+         --request POST ${URL} \
+         --header 'Content-Type: application/json' \
+         --data-raw '{
+        "query": "'"${sql}"'"
+    }'
+}
+
+ceresdb_query 'CREATE TABLE `demo` (`name` string TAG, `value` double NOT NULL, `t` timestamp NOT NULL, TIMESTAMP KEY(t)) ENGINE=Analytic with (enable_ttl='\''false'\'')'
+
+ceresdb_query 'INSERT INTO demo(t, name, value) VALUES(1651737067000, '\''ceresdb'\'', 100)'
+
+ceresdb_query 'select * from demo'
+
+ceresdb_query 'show create table demo'
+
+ceresdb_query 'DROP TABLE demo'
