ceb2668659cdfca7e6dc8280fc723368210ed76d
dust1 <834902408@qq.com>
2022-08-07 22:27:56 +0800

    fix:select system.public.tables will return error

--- system_catalog/src/tables.rs
+++ system_catalog/src/tables.rs
@@ -138,8 +138,8 @@ impl<M: Manager> SystemTable for Tables<M> {
             .all_catalogs()
             .map_err(|e| Box::new(e) as _)
             .context(table_engine::table::Scan { table: self.name() })?;
-        let mut builder =
-            RecordBatchWithKeyBuilder::new(self.schema.clone().to_record_schema_with_key());
+        let record_schema_key = request.projected_schema.to_record_schema_with_key();
+        let mut builder = RecordBatchWithKeyBuilder::new(record_schema_key);
 
         let projector = request
             .projected_schema
