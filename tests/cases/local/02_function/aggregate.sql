9fc7f8642b514ca74c2493fc16dac4e6ffa6a43d
chunshao.rcs <chunshao.rcs@antgroup.com>
2022-09-27 15:48:47 +0800

    chore: fix typo

--- tests/cases/local/02_function/aggregate.sql
+++ tests/cases/local/02_function/aggregate.sql
@@ -1,6 +1,6 @@
-DROP TABLE IF EXISTS `02_function_aggretate_table1`;
+DROP TABLE IF EXISTS `02_function_aggregate_table1`;
 
-CREATE TABLE `02_function_aggretate_table1` (
+CREATE TABLE `02_function_aggregate_table1` (
     `timestamp` timestamp NOT NULL,
     `arch` string TAG,
     `datacenter` string TAG,
@@ -11,7 +11,7 @@ WITH(
 	 enable_ttl='false'
 );
 
-INSERT INTO `02_function_aggretate_table1`
+INSERT INTO `02_function_aggregate_table1`
     (`timestamp`, `arch`, `datacenter`, `value`, `uvalue`)
 VALUES
     (1658304762, 'x86-64', 'china', 100, 10),
@@ -20,13 +20,13 @@ VALUES
     (1658304763, 'arm64', 'china', 210, 0);
 
 
-SELECT sum(`value`) FROM `02_function_aggretate_table1`;
+SELECT sum(`value`) FROM `02_function_aggregate_table1`;
 
 SELECT
     `arch`,
     sum(`value`)
 FROM
-    `02_function_aggretate_table1`
+    `02_function_aggregate_table1`
 WHERE
     `timestamp` BETWEEN 1658304763 AND 1658304763
 GROUP BY
@@ -35,24 +35,24 @@ ORDER BY
     `arch` DESC;
 
 
-SELECT count(`value`) FROM `02_function_aggretate_table1`;
+SELECT count(`value`) FROM `02_function_aggregate_table1`;
 
-SELECT avg(`value`) FROM `02_function_aggretate_table1`;
+SELECT avg(`value`) FROM `02_function_aggregate_table1`;
 
-SELECT max(`value`) FROM `02_function_aggretate_table1`;
+SELECT max(`value`) FROM `02_function_aggregate_table1`;
 
-SELECT min(`value`) FROM `02_function_aggretate_table1`;
+SELECT min(`value`) FROM `02_function_aggregate_table1`;
 
-SELECT min(`uvalue`) - max(`uvalue`) FROM `02_function_aggretate_table1`;
+SELECT min(`uvalue`) - max(`uvalue`) FROM `02_function_aggregate_table1`;
 
 -- duplicate with last insert
-INSERT INTO `02_function_aggretate_table1`
+INSERT INTO `02_function_aggregate_table1`
     (`timestamp`, `arch`, `datacenter`, `value`)
 VALUES
     (1658304762, 'x86-64', 'china', 100);
 
-SELECT count(`arch`) FROM `02_function_aggretate_table1`;
+SELECT count(`arch`) FROM `02_function_aggregate_table1`;
 
-SELECT count(distinct(`arch`)) FROM `02_function_aggretate_table1`;
+SELECT count(distinct(`arch`)) FROM `02_function_aggregate_table1`;
 
-DROP TABLE `02_function_aggretate_table1`;
+DROP TABLE `02_function_aggregate_table1`;
