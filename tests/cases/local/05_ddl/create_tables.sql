1165a7e9dd5d8399edc26e815188f8ab02df47d0
jiacai2050 <dev@liujiacai.net>
2022-10-19 09:15:04 +0800

    ignore 05_create_tables_t8 testcase

--- tests/cases/local/05_ddl/create_tables.sql
+++ tests/cases/local/05_ddl/create_tables.sql
@@ -61,7 +61,8 @@ CREATE TABLE `05_create_tables_t8`(c1 int, t1 timestamp NOT NULL TIMESTAMP KEY) 
 show create table `05_create_tables_t8`;
 drop table `05_create_tables_t8`;
 
-CREATE TABLE `05_create_tables_t8`(c1 int, t1 timestamp NOT NULL TIMESTAMP KEY) ENGINE = Analytic with (storage_format= 'unknown');
+-- Ignore now, table_id is not stable now
+-- CREATE TABLE `05_create_tables_t8`(c1 int, t1 timestamp NOT NULL TIMESTAMP KEY) ENGINE = Analytic with (storage_format= 'unknown');
 
 -- Default value options
 CREATE TABLE `05_create_tables_t9`(c1 int, c2 bigint default 0, c3 uint32 default 1 + 1, c4 string default 'xxx', c5 uint32 default c3*2 + 1, t1 timestamp NOT NULL TIMESTAMP KEY) ENGINE = Analytic;
