e16ceb92f8d562ccd428f540ebefd4611d166adb
kamille <34352236+Rachelint@users.noreply.github.com>
2022-11-07 17:41:55 +0800

    feat: region in wal's message queue impl.   (#362)
    * add `get_table_meta_data` method into `RegionMeta`.
    
    * add region module.
    
    * remove unused in region meta.
    
    * make clippy happy.
    
    * address CR.

--- wal/src/message_queue_impl/mod.rs
+++ wal/src/message_queue_impl/mod.rs
@@ -2,5 +2,26 @@
 
 //! Wal manager based on message queue
 
-pub(crate) mod encoding;
-pub(crate) mod region_meta;
+use std::collections::BTreeMap;
+
+use chrono::Utc;
+use message_queue::Message;
+
+mod encoding;
+mod log_cleaner;
+mod region;
+mod region_meta;
+mod snapshot_synchronizer;
+
+#[cfg(test)]
+mod test_util;
+
+#[inline]
+fn to_message(log_key: Vec<u8>, log_value: Vec<u8>) -> Message {
+    Message {
+        key: Some(log_key),
+        value: Some(log_value),
+        headers: BTreeMap::default(),
+        timestamp: Utc::now(),
+    }
+}
