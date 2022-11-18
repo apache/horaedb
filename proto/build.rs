96c8dc6a278af7cfd8d6f7a7d0f9fc033a568d84
ShiKaiWi <xykwei@gmail.com>
2022-11-18 13:54:08 +0800

    chore: remove manual installation of protoc

--- proto/build.rs
+++ proto/build.rs
@@ -1,6 +1,10 @@
 // Copyright 2022 CeresDB Project Authors. Licensed under Apache-2.0.
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Download the protoc and set the path for tonic_build.
+    let protoc_path = protoc_bin_vendored::protoc_bin_path().map_err(|e| Box::new(e))?;
+    std::env::set_var("PROTOC", protoc_path.as_os_str());
+
     tonic_build::configure().out_dir("./src").compile(
         &[
             "protos/analytic_common.proto",
