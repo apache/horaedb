60430b563dfc1a76d7fd480222786d474e263a8c
ShiKaiWi <xykwei@gmail.com>
2022-11-18 14:13:45 +0800

    make clippy happy

--- proto/build.rs
+++ proto/build.rs
@@ -2,7 +2,7 @@
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Download the protoc and set the path for tonic_build.
-    let protoc_path = protoc_bin_vendored::protoc_bin_path().map_err(|e| Box::new(e))?;
+    let protoc_path = protoc_bin_vendored::protoc_bin_path().map_err(Box::new)?;
     std::env::set_var("PROTOC", protoc_path.as_os_str());
 
     tonic_build::configure().out_dir("./src").compile(
