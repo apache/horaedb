437dcab77834b323e737f638b41aeaa013544b4b
kamille <caoruiqiu.crq@antgroup.com>
2022-12-21 16:05:55 +0800

    define partition rule trait.

--- table_engine/src/partition/rule.rs
+++ table_engine/src/partition/rule.rs
@@ -2,7 +2,9 @@
 
 //! Partition rules
 
+use common_types::row::RowGroup;
 use common_util::define_result;
+use datafusion_expr::{Expr, Operator};
 use snafu::Snafu;
 
 #[derive(Debug, Snafu)]
@@ -11,4 +13,25 @@ pub enum Error {}
 define_result!(Error);
 
 /// Partition rule locate partition
-pub trait PartitionRule {}
+pub trait PartitionRule {
+    /// Locate the partition for each row in `row_group`
+    ///
+    /// Len of returned value should be equal to the one of rows in `row group`.
+    fn locate_partitions_for_write(row_group: &RowGroup) -> Vec<usize>;
+
+    /// locate partitions according to `filters`
+    fn locate_partitions_for_read(filters: &[PartitionFilter]) -> Vec<usize>;
+}
+
+/// Filter using for partition
+///
+/// Now, it is same as the `BinaryExpr`in datafusion.
+#[allow(dead_code)]
+pub struct PartitionFilter {
+    /// Left-hand side of the expression
+    left: Box<Expr>,
+    /// The comparison operator
+    op: Operator,
+    /// Right-hand side of the expression
+    right: Box<Expr>,
+}
