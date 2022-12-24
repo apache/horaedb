eca901e439c7204184ac67b5345a876f334eb779
kamille <caoruiqiu.crq@antgroup.com>
2022-12-24 17:43:30 +0800

    add tests for key partition.

--- table_engine/src/partition/rule/key.rs
+++ table_engine/src/partition/rule/key.rs
@@ -10,7 +10,8 @@ use common_types::{
     hash::hash64,
     row::{Row, RowGroup},
 };
-use itertools::{iproduct, Itertools};
+use itertools::Itertools;
+use log::debug;
 use snafu::OptionExt;
 
 use crate::partition::{
@@ -35,6 +36,8 @@ impl KeyRule {
             .map(|(col_idx, typed_col)| (typed_col.column.clone(), col_idx))
             .collect::<HashMap<_, _>>();
         let mut filter_by_columns = vec![Vec::new(); self.typed_columns.len()];
+
+        // Group the filters by their columns.
         for (filter_idx, filter) in filters.iter().enumerate() {
             let col_idx = column_name_to_idxs
                 .get(filter.column.as_str())
@@ -50,14 +53,36 @@ impl KeyRule {
                 .unwrap()
                 .push(filter_idx);
         }
+        debug!(
+            "KeyRule get candidate partition keys groups, filter_by_columns:{:?}",
+            filter_by_columns
+        );
 
         // Check and do cartesian product to get candidate partition groups.
+        // for example:
+        //      key_col1: [f1, f2, f3]
+        //      key_col2: [f4, f5]
+        // will convert to:
+        //      group1: [key_col1: f1, key_col2: f4]
+        //      group2: [key_col1: f1, key_col2: f5]
+        //      group3: [key_col1: f2, key_col2: f4]
+        //      ...
         let empty_filter = filter_by_columns.iter().find(|filter| filter.is_empty());
         if empty_filter.is_some() {
             return Ok(Vec::default());
         }
 
-        Ok(iproduct!(filter_by_columns).collect())
+        let groups = filter_by_columns
+            .into_iter()
+            .map(|filters| filters.into_iter())
+            .multi_cartesian_product()
+            .collect_vec();
+        debug!(
+            "KeyRule get candidate partition keys groups, groups:{:?}",
+            groups
+        );
+
+        Ok(groups)
     }
 
     fn compute_partition_for_inserted_row(
@@ -144,8 +169,9 @@ fn expand_partition_keys_group(
         let filter = &filters[filter_idx];
         let datums = match &filter.condition {
             // Only `Eq` is supported now.
-            // TODO: support `In`.
+            // TODO: to support `In`'s extracting.
             PartitionCondition::Eq(datum) => vec![datum.clone()],
+            PartitionCondition::In(datums) => datums.clone(),
             _ => {
                 return LocateReadPartition {
                     msg: format!("invalid partition filter found, filter:{:?},", filter),
@@ -157,10 +183,15 @@ fn expand_partition_keys_group(
         datum_by_columns.push(datums);
     }
 
-    Ok(iproduct!(datum_by_columns).collect())
+    let expanded_group = datum_by_columns
+        .into_iter()
+        .map(|filters| filters.into_iter())
+        .multi_cartesian_product()
+        .collect_vec();
+    Ok(expanded_group)
 }
 
-// Helper function for computing partition
+// Compute partition
 fn compute_partition(partition_keys: &[&Datum], partition_num: u64, buf: &mut BytesMut) -> usize {
     buf.clear();
     partition_keys
@@ -172,6 +203,8 @@ fn compute_partition(partition_keys: &[&Datum], partition_num: u64, buf: &mut By
 
 #[cfg(test)]
 mod tests {
+    use std::collections::BTreeSet;
+
     use common_types::{datum::DatumKind, string::StringBytes};
 
     use super::*;
@@ -207,5 +240,107 @@ mod tests {
         let expected = (hash64(&buf) % partition_num) as usize;
 
         assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_get_candidate_partition_keys_groups() {
+        // Key rule of keys:[col1, col2, col3]
+        let partition_num = 16;
+        let key_rule = KeyRule {
+            typed_columns: vec![
+                ColumnWithType::new("col1".to_string(), DatumKind::UInt32),
+                ColumnWithType::new("col2".to_string(), DatumKind::UInt32),
+                ColumnWithType::new("col3".to_string(), DatumKind::UInt32),
+            ],
+            partition_num,
+        };
+
+        // Filters(related columns: col1, col2, col3, col1, col2)
+        let filter1 = PartitionFilter {
+            column: "col1".to_string(),
+            condition: PartitionCondition::Eq(Datum::UInt32(1)),
+        };
+
+        let filter2 = PartitionFilter {
+            column: "col2".to_string(),
+            condition: PartitionCondition::Eq(Datum::UInt32(2)),
+        };
+
+        let filter3 = PartitionFilter {
+            column: "col3".to_string(),
+            condition: PartitionCondition::Eq(Datum::UInt32(3)),
+        };
+
+        let filter4 = PartitionFilter {
+            column: "col1".to_string(),
+            condition: PartitionCondition::Eq(Datum::UInt32(4)),
+        };
+
+        let filter5 = PartitionFilter {
+            column: "col2".to_string(),
+            condition: PartitionCondition::Eq(Datum::UInt32(5)),
+        };
+
+        let filters = vec![filter1, filter2, filter3, filter4, filter5];
+
+        // Groups' len: 4
+        // Col1's filter idxs: [0, 3]
+        // Col2's filter idxs: [1, 4]
+        // Col3's filter idxs: [2]
+        let groups = key_rule
+            .get_candidate_partition_keys_groups(&filters)
+            .unwrap();
+        assert_eq!(groups.len(), 4);
+
+        let mut filter_in_groups = vec![BTreeSet::new(); 3];
+        for group in groups {
+            filter_in_groups[0].insert(group[0]);
+            filter_in_groups[1].insert(group[1]);
+            filter_in_groups[2].insert(group[2]);
+        }
+        let filter_in_groups = filter_in_groups
+            .into_iter()
+            .map(|filters| filters.into_iter().collect_vec())
+            .collect_vec();
+
+        assert_eq!(filter_in_groups[0], [0, 3]);
+        assert_eq!(filter_in_groups[1], [1, 4]);
+        assert_eq!(filter_in_groups[2], [2]);
+    }
+
+    #[test]
+    fn test_expand_partition_keys_group() {
+        // Filters(related columns: col1, col2, col3, col1)
+        let filter1 = PartitionFilter {
+            column: "col1".to_string(),
+            condition: PartitionCondition::Eq(Datum::UInt32(1)),
+        };
+
+        let filter2 = PartitionFilter {
+            column: "col2".to_string(),
+            condition: PartitionCondition::In(vec![Datum::UInt32(2), Datum::UInt32(22)]),
+        };
+
+        let filter3 = PartitionFilter {
+            column: "col3".to_string(),
+            condition: PartitionCondition::Eq(Datum::UInt32(3)),
+        };
+
+        let filter4 = PartitionFilter {
+            column: "col1".to_string(),
+            condition: PartitionCondition::Eq(Datum::UInt32(4)),
+        };
+        let filters = vec![filter1, filter2, filter3, filter4];
+
+        // Group
+        let group = vec![0, 1, 2];
+
+        // Expanded group
+        let expanded_group = expand_partition_keys_group(group, &filters).unwrap();
+        let expected = vec![
+            vec![Datum::UInt32(1), Datum::UInt32(2), Datum::UInt32(3)],
+            vec![Datum::UInt32(1), Datum::UInt32(22), Datum::UInt32(3)],
+        ];
+        assert_eq!(expanded_group, expected);
     }
 }
