96838fc3a747c4b9ab93e6cb5da01ebc862ab865
jiacai2050 <dev@liujiacai.net>
2023-01-12 18:14:32 +0800

    add comments

--- server/src/error_util.rs
+++ server/src/error_util.rs
@@ -1,5 +1,9 @@
 // Copyright 2022 CeresDB Project Authors. Licensed under Apache-2.0.
 
+// Our backtrace is defined like this
+// #[snafu(display("Time range is not found.\nBacktrace\n:{}", backtrace))]
+//
+// So here we split by `Backtrace`, and return first part
 pub fn remove_backtrace_from_err(err_string: &str) -> &str {
     err_string
         .splitn(2, "Backtrace")
@@ -15,10 +19,12 @@ mod tests {
     fn test_remove_backtrace() {
         let cases = vec![
             (
-                r#"Failed to execute select, err:Failed to execute logical plan, err:Failed to do physical optimization, err:DataFusion Failed to optimize physical plan, err:Error during planning.
+                r#"Failed to execute select, err:Failed to execute logical plan, err:Failed to do physical optimization,
+err:DataFusion Failed to optimize physical plan, err:Error during planning.
 Backtrace:
  0 <snafu::backtrace_shim::Backtrace as snafu::GenerateBacktrace>::generate::h996ee016dfa35e37"#,
-                r#"Failed to execute select, err:Failed to execute logical plan, err:Failed to do physical optimization, err:DataFusion Failed to optimize physical plan, err:Error during planning.
+                r#"Failed to execute select, err:Failed to execute logical plan, err:Failed to do physical optimization,
+err:DataFusion Failed to optimize physical plan, err:Error during planning.
 "#,
             ),
             ("", ""),
