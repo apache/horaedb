9470dfba8779b25a77c77b75856a9ab66ae62222
WEI Xikai <ShiKaiWi@users.noreply.github.com>
2023-01-12 17:33:16 +0800

    refactor: separate object store from async reader (#562)
    * refactor: separate object store from parquet sst async reader
    
    * fix storage format in integration tests
    
    * move fetch_meta_data to parquet_ext crate
    
    * move AsyncFileReader to separate mod
    
    * rename some structs

---
+++ analytic_engine/src/sst/file_reader.rs
@@ -0,0 +1,75 @@
+// Copyright 2022 CeresDB Project Authors. Licensed under Apache-2.0.
+
+use std::{
+    ops::Range,
+    sync::{Arc, RwLock},
+};
+
+use async_trait::async_trait;
+use bytes::Bytes;
+use common_util::error::{GenericError, GenericResult};
+use object_store::{ObjectStoreRef, Path};
+
+pub type FileChunkReaderRef = Arc<dyn FileChunkReader>;
+
+#[async_trait]
+pub trait FileChunkReader: Send + Sync {
+    async fn file_size(&self) -> GenericResult<usize>;
+
+    async fn get_byte_range(&self, range: Range<usize>) -> GenericResult<Bytes>;
+
+    async fn get_byte_ranges(&self, ranges: &[Range<usize>]) -> GenericResult<Vec<Bytes>>;
+}
+
+/// A [`FileChunkReader`] implementation based on [`ObjectStore`].
+pub struct FileChunkReaderOnObjectStore {
+    path: Path,
+    store: ObjectStoreRef,
+    cached_file_size: RwLock<Option<usize>>,
+}
+
+impl FileChunkReaderOnObjectStore {
+    pub fn new(path: Path, store: ObjectStoreRef) -> Self {
+        Self {
+            path,
+            store,
+            cached_file_size: RwLock::new(None),
+        }
+    }
+}
+
+#[async_trait]
+impl FileChunkReader for FileChunkReaderOnObjectStore {
+    async fn file_size(&self) -> GenericResult<usize> {
+        // check cached filed_size first
+        {
+            let file_size = self.cached_file_size.read().unwrap();
+            if let Some(s) = file_size.as_ref() {
+                return Ok(*s);
+            }
+        }
+
+        // fetch the size from the underlying store
+        let head = self
+            .store
+            .head(&self.path)
+            .await
+            .map_err(|e| Box::new(e) as GenericError)?;
+        *self.cached_file_size.write().unwrap() = Some(head.size);
+        Ok(head.size)
+    }
+
+    async fn get_byte_range(&self, range: Range<usize>) -> GenericResult<Bytes> {
+        self.store
+            .get_range(&self.path, range)
+            .await
+            .map_err(|e| Box::new(e) as _)
+    }
+
+    async fn get_byte_ranges(&self, ranges: &[Range<usize>]) -> GenericResult<Vec<Bytes>> {
+        self.store
+            .get_ranges(&self.path, ranges)
+            .await
+            .map_err(|e| Box::new(e) as _)
+    }
+}
--- components/parquet_ext/src/lib.rs
+++ components/parquet_ext/src/lib.rs
@@ -1,5 +1,6 @@
 // Copyright 2022 CeresDB Project Authors. Licensed under Apache-2.0.
 
+pub mod meta_data;
 pub mod prune;
 pub mod reverse_reader;
 #[cfg(test)]
