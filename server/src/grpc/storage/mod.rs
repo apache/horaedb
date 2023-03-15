053738cfe86bf9f037b7db9a6ad4142d73fffc12
chunshao.rcs <chunshao.rcs@antgroup.com>
2023-03-15 12:46:06 +0800

    feat: impl stream write with proxy

--- server/src/grpc/storage/mod.rs
+++ server/src/grpc/storage/mod.rs
@@ -1,21 +1,31 @@
 // Copyright 2023 CeresDB Project Authors. Licensed under Apache-2.0.
 
 mod error;
-mod metrics;
 
-use std::{sync::Arc, time::Duration};
+use std::{
+    sync::Arc,
+    time::{Duration, Instant},
+};
 
 use async_trait::async_trait;
-use ceresdbproto::storage::{
-    storage_service_server::StorageService, PrometheusQueryRequest, PrometheusQueryResponse,
-    RouteRequest, RouteResponse, SqlQueryRequest, SqlQueryResponse, WriteRequest, WriteResponse,
+use ceresdbproto::{
+    common::ResponseHeader,
+    storage::{
+        storage_service_server::StorageService, PrometheusQueryRequest, PrometheusQueryResponse,
+        RouteRequest, RouteResponse, SqlQueryRequest, SqlQueryResponse, WriteRequest,
+        WriteResponse,
+    },
 };
-use futures::stream::BoxStream;
+use common_util::time::InstantExt;
+use futures::{stream::BoxStream, StreamExt};
 use http::StatusCode;
 use query_engine::executor::Executor as QueryExecutor;
 use table_engine::engine::EngineRuntimes;
 
-use crate::proxy::{Context, Proxy};
+use crate::{
+    grpc::metrics::GRPC_HANDLER_DURATION_HISTOGRAM_VEC,
+    proxy::{Context, Proxy},
+};
 
 #[derive(Clone)]
 pub struct StorageServiceImpl<Q: QueryExecutor + 'static> {
@@ -61,7 +71,14 @@ impl<Q: QueryExecutor + 'static> StorageService for StorageServiceImpl<Q> {
         &self,
         request: tonic::Request<tonic::Streaming<WriteRequest>>,
     ) -> std::result::Result<tonic::Response<WriteResponse>, tonic::Status> {
-        self.stream_write_internal(request).await
+        let begin_instant = Instant::now();
+
+        let resp = self.stream_write_internal(request).await;
+
+        GRPC_HANDLER_DURATION_HISTOGRAM_VEC
+            .handle_stream_write
+            .observe(begin_instant.saturating_elapsed().as_secs_f64());
+        resp
     }
 
     async fn stream_sql_query(
@@ -187,8 +204,46 @@ impl<Q: QueryExecutor + 'static> StorageServiceImpl<Q> {
 
     async fn stream_write_internal(
         &self,
-        _request: tonic::Request<tonic::Streaming<WriteRequest>>,
+        req: tonic::Request<tonic::Streaming<WriteRequest>>,
     ) -> std::result::Result<tonic::Response<WriteResponse>, tonic::Status> {
-        todo!()
+        let mut total_success = 0;
+        let mut resp = WriteResponse::default();
+        let mut has_err = false;
+        let mut stream = req.into_inner();
+        while let Some(req) = stream.next().await {
+            let write_req = match req {
+                Ok(v) => v,
+                Err(e) => {
+                    return Ok(tonic::Response::new(WriteResponse {
+                        header: Some(error::build_err_header(
+                            StatusCode::INTERNAL_SERVER_ERROR.as_u16() as u32,
+                            format!("fail to fetch request, err:{e:?}"),
+                        )),
+                        ..Default::default()
+                    }));
+                }
+            };
+
+            let write_resp = self.proxy.handle_write(Context::default(), write_req).await;
+
+            if let Some(header) = write_resp.header {
+                if header.code != StatusCode::OK.as_u16() as u32 {
+                    resp.header = Some(header);
+                    has_err = true;
+                    break;
+                }
+            }
+            total_success += write_resp.success;
+        }
+
+        if !has_err {
+            resp.header = Some(ResponseHeader {
+                code: StatusCode::OK.as_u16() as u32,
+                ..Default::default()
+            });
+            resp.success = total_success;
+        }
+
+        Ok(tonic::Response::new(resp))
     }
 }
