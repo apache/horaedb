a056466afaa652e45cd774061a4e77a67da1c2b0
xikai.wxk <xikai.wxk@antgroup.com>
2023-04-20 18:07:09 +0800

    feat: enable cluster integration tests

--- integration_tests/build_meta.sh
+++ integration_tests/build_meta.sh
@@ -3,7 +3,7 @@
 set -exo
 
 if [ ! -d ceresmeta ]; then
-    git clone --depth 1 https://github.com/ceresdb/ceresmeta.git
+    git clone --depth 1 https://github.com/ceresdb/ceresmeta.git --branch refactor_cluster
 fi
 cd ceresmeta
 go build -o ceresmeta ./cmd/meta/...
