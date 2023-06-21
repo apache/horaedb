c32ebd137f8d5b698fd4d568cd479970eb1083d6
tanruixiang <tanruixiang0104@gmail.com>
2023-06-21 13:48:28 +0800

    Revert "support get ScalarValue from dictionary type"
    This reverts commit c8f7c815b82fddfc82cf67f744e3ca4b11cf4dfd.

--- common_types/src/datum.rs
+++ common_types/src/datum.rs
@@ -1209,7 +1209,6 @@ pub mod arrow_convert {
                 }
                 ScalarValue::Date32(v) => v.map(Datum::Date),
                 ScalarValue::Time64Nanosecond(v) => v.map(Datum::Time),
-                ScalarValue::Dictionary(_, literal) => Datum::from_scalar_value(literal),
                 ScalarValue::List(_, _)
                 | ScalarValue::Date64(_)
                 | ScalarValue::Time32Second(_)
@@ -1223,7 +1222,8 @@ pub mod arrow_convert {
                 | ScalarValue::Struct(_, _)
                 | ScalarValue::Decimal128(_, _, _)
                 | ScalarValue::Null
-                | ScalarValue::IntervalMonthDayNano(_) => None,
+                | ScalarValue::IntervalMonthDayNano(_)
+                | ScalarValue::Dictionary(_, _) => None,
             }
         }
     }
@@ -1255,7 +1255,6 @@ pub mod arrow_convert {
                 ScalarValue::TimestampMillisecond(v, _) => {
                     v.map(|v| DatumView::Timestamp(Timestamp::new(v)))
                 }
-                ScalarValue::Dictionary(_, literal) => DatumView::from_scalar_value(&literal),
                 ScalarValue::List(_, _)
                 | ScalarValue::Date64(_)
                 | ScalarValue::Time32Second(_)
@@ -1269,7 +1268,8 @@ pub mod arrow_convert {
                 | ScalarValue::Struct(_, _)
                 | ScalarValue::Decimal128(_, _, _)
                 | ScalarValue::Null
-                | ScalarValue::IntervalMonthDayNano(_) => None,
+                | ScalarValue::IntervalMonthDayNano(_)
+                | ScalarValue::Dictionary(_, _) => None,
             }
         }
     }
--- common_types/src/record_batch.rs
+++ common_types/src/record_batch.rs
@@ -478,7 +478,11 @@ impl RecordBatchWithKeyBuilder {
             .columns()
             .iter()
             .map(|column_schema| {
-                ColumnBlockBuilder::with_capacity(&column_schema.data_type, 0, column_schema.is_tag)
+                ColumnBlockBuilder::with_capacity(
+                    &column_schema.data_type,
+                    0,
+                    column_schema.is_tag,
+                )
             })
             .collect();
         Self {
