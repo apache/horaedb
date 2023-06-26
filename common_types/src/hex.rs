7430fe708246bc2857a57bf788df161b51b5211c
xikai.wxk <xikai.wxk@antgroup.com>
2023-06-26 19:58:02 +0800

    fix: support hex string literal with odd number of chars

--- common_types/src/hex.rs
+++ common_types/src/hex.rs
@@ -8,12 +8,16 @@
 /// None will be returned if the input literal is hex-invalid.
 pub fn try_decode(s: &str) -> Option<Vec<u8>> {
     let hex_bytes = s.as_bytes();
-    if hex_bytes.len() % 2 != 0 {
-        return None;
+
+    let mut decoded_bytes = Vec::with_capacity((hex_bytes.len() + 1) / 2);
+
+    let start_idx = hex_bytes.len() % 2;
+    if start_idx > 0 {
+        // The first byte is formed of only one char.
+        decoded_bytes.push(try_decode_hex_char(hex_bytes[0])?);
     }
 
-    let mut decoded_bytes = Vec::with_capacity(hex_bytes.len() / 2);
-    for i in (0..hex_bytes.len()).step_by(2) {
+    for i in (start_idx..hex_bytes.len()).step_by(2) {
         let high = try_decode_hex_char(hex_bytes[i])?;
         let low = try_decode_hex_char(hex_bytes[i + 1])?;
         decoded_bytes.push(high << 4 | low);
@@ -44,10 +48,12 @@ mod tests {
             ("", Some(vec![])),
             ("FF00", Some(vec![255, 0])),
             ("a00a", Some(vec![160, 10])),
-            ("FF0", None),
+            ("FF0", Some(vec![15, 240])),
+            ("f", Some(vec![15])),
             ("FF0X", None),
             ("X0", None),
             ("XX", None),
+            ("x", None),
         ];
 
         for (input, expect) in cases {
