040b68a4f5c434e8d5900db8e22b0931b4bfde93
chunshao.rcs <chunshao.rcs@antgroup.com>
2023-07-20 22:51:56 +0800

    impl some types stored in Column

--- common_types/src/column.rs
+++ common_types/src/column.rs
@@ -15,6 +15,7 @@ use arrow::{
     datatypes::DataType,
     error::ArrowError,
 };
+use arrow::array::{Float32Array, Int16Array, Int8Array, UInt16Array, UInt32Array, UInt8Array};
 use bytes_ext::{BufMut, Bytes, BytesMut};
 use ceresdbproto::storage::value;
 use datafusion::parquet::data_type::AsBytes;
@@ -71,9 +72,15 @@ impl IntoIterator for Column {
 #[allow(missing_docs)]
 pub enum ColumnData {
     F64(Vec<f64>),
+    F32(Vec<f32>),
     I64(Vec<i64>),
     I32(Vec<i32>),
+    I16(Vec<i16>),
+    I8(Vec<i8>),
     U64(Vec<u64>),
+    U32(Vec<u32>),
+    U16(Vec<u16>),
+    U8(Vec<u8>),
     String(Vec<String>),
     StringBytes(Vec<StringBytes>),
     Varbinary(Vec<Vec<u8>>),
@@ -84,8 +91,15 @@ impl std::fmt::Display for ColumnData {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::F64(col_data) => write!(f, "F64({})", col_data.len()),
+            Self::F32(col_data) => write!(f, "F32({})", col_data.len()),
             Self::I64(col_data) => write!(f, "I64({})", col_data.len()),
+            Self::I32(col_data) => write!(f, "I32({})", col_data.len()),
+            Self::I16(col_data) => write!(f, "I16({})", col_data.len()),
+            Self::I8(col_data) => write!(f, "I8({})", col_data.len()),
             Self::U64(col_data) => write!(f, "U64({})", col_data.len()),
+            Self::U32(col_data) => write!(f, "U32({})", col_data.len()),
+            Self::U16(col_data) => write!(f, "U16({})", col_data.len()),
+            Self::U8(col_data) => write!(f, "U8({})", col_data.len()),
             Self::String(col_data) => write!(f, "String({})", col_data.len()),
             Self::Varbinary(col_data) => write!(f, "Varbinary({})", col_data.len()),
             Self::Bool(col_data) => write!(f, "Bool({})", col_data.len()),
@@ -96,9 +110,15 @@ impl std::fmt::Display for ColumnData {
 
 pub enum ColumnDataIter {
     F64(std::vec::IntoIter<f64>),
+    F32(std::vec::IntoIter<f32>),
     I64(std::vec::IntoIter<i64>),
     I32(std::vec::IntoIter<i32>),
+    I16(std::vec::IntoIter<i16>),
+    I8(std::vec::IntoIter<i8>),
     U64(std::vec::IntoIter<u64>),
+    U32(std::vec::IntoIter<u32>),
+    U16(std::vec::IntoIter<u16>),
+    U8(std::vec::IntoIter<u8>),
     String(std::vec::IntoIter<String>),
     StringBytes(std::vec::IntoIter<StringBytes>),
     Varbinary(std::vec::IntoIter<Vec<u8>>),
@@ -111,9 +131,15 @@ impl<'a> Iterator for ColumnDataIter {
     fn next(&mut self) -> Option<Self::Item> {
         match self {
             Self::F64(col_data) => col_data.next().map(|x| value::Value::Float64Value(x)),
+            Self::F32(col_data) => col_data.next().map(|x| value::Value::Float32Value(x)),
             Self::I64(col_data) => col_data.next().map(|x| value::Value::Int64Value(x)),
             Self::I32(col_data) => col_data.next().map(|x| value::Value::Int32Value(x)),
+            Self::I16(col_data) => col_data.next().map(|x| value::Value::Int16Value(x as i32)),
+            Self::I8(col_data) => col_data.next().map(|x| value::Value::Int8Value(x as i32)),
             Self::U64(col_data) => col_data.next().map(|x| value::Value::Uint64Value(x)),
+            Self::U32(col_data) => col_data.next().map(|x| value::Value::Uint32Value(x)),
+            Self::U16(col_data) => col_data.next().map(|x| value::Value::Uint16Value(x as u32)),
+            Self::U8(col_data) => col_data.next().map(|x| value::Value::Uint8Value(x as u32)),
             Self::String(col_data) => col_data.next().map(|x| value::Value::StringValue(x)),
             Self::StringBytes(col_data) => col_data
                 .next()
@@ -131,9 +157,15 @@ impl IntoIterator for ColumnData {
     fn into_iter(self) -> Self::IntoIter {
         match self {
             Self::F64(col_data) => ColumnDataIter::F64(col_data.into_iter()),
+            Self::F32(col_data) => ColumnDataIter::F32(col_data.into_iter()),
             Self::I64(col_data) => ColumnDataIter::I64(col_data.into_iter()),
             Self::I32(col_data) => ColumnDataIter::I32(col_data.into_iter()),
+            Self::I16(col_data) => ColumnDataIter::I16(col_data.into_iter()),
+            Self::I8(col_data) => ColumnDataIter::I8(col_data.into_iter()),
             Self::U64(col_data) => ColumnDataIter::U64(col_data.into_iter()),
+            Self::U32(col_data) => ColumnDataIter::U32(col_data.into_iter()),
+            Self::U16(col_data) => ColumnDataIter::U16(col_data.into_iter()),
+            Self::U8(col_data) => ColumnDataIter::U8(col_data.into_iter()),
             Self::String(col_data) => ColumnDataIter::String(col_data.into_iter()),
             Self::StringBytes(col_data) => ColumnDataIter::StringBytes(col_data.into_iter()),
             Self::Varbinary(col_data) => ColumnDataIter::Varbinary(col_data.into_iter()),
@@ -155,9 +187,15 @@ impl Column {
                 ColumnData::Bool(data)
             }
             DatumKind::UInt64 => ColumnData::U64(vec![0; row_count]),
+            DatumKind::UInt32 => ColumnData::U32(vec![0; row_count]),
+            DatumKind::UInt16 => ColumnData::U16(vec![0; row_count]),
+            DatumKind::UInt8 => ColumnData::U8(vec![0; row_count]),
             DatumKind::Double => ColumnData::F64(vec![0.0; row_count]),
+            DatumKind::Float => ColumnData::F32(vec![0.0; row_count]),
             DatumKind::Int64 | DatumKind::Timestamp => ColumnData::I64(vec![0; row_count]),
             DatumKind::Int32 => ColumnData::I32(vec![0; row_count]),
+            DatumKind::Int16 => ColumnData::I16(vec![0; row_count]),
+            DatumKind::Int8 => ColumnData::I8(vec![0; row_count]),
             // DatumKind::String => ColumnData::String(vec!["".to_string(); row_count]),
             DatumKind::String => ColumnData::StringBytes(vec![StringBytes::new(); row_count]),
             DatumKind::Varbinary => ColumnData::Varbinary(vec![vec![]; row_count]),
@@ -182,13 +220,31 @@ impl Column {
             (ColumnData::F64(data), ColumnData::F64(ref mut column_data)) => {
                 data.append(column_data)
             }
+            (ColumnData::F32(data), ColumnData::F32(ref mut column_data)) => {
+                data.append(column_data)
+            }
             (ColumnData::I64(data), ColumnData::I64(ref mut column_data)) => {
                 data.append(column_data)
             }
             (ColumnData::I32(data), ColumnData::I32(ref mut column_data)) => {
                 data.append(column_data)
             }
+            (ColumnData::I16(data), ColumnData::I16(ref mut column_data)) => {
+                data.append(column_data)
+            }
+            (ColumnData::I8(data), ColumnData::I8(ref mut column_data)) => {
+                data.append(column_data)
+            }
             (ColumnData::U64(data), ColumnData::U64(ref mut column_data)) => {
+                data.append(column_data)
+            }
+            (ColumnData::U32(data), ColumnData::U32(ref mut column_data)) => {
+                data.append(column_data)
+            }
+            (ColumnData::U16(data), ColumnData::U16(ref mut column_data)) => {
+                data.append(column_data)
+            }
+            (ColumnData::U8(data), ColumnData::U8(ref mut column_data)) => {
                 data.append(column_data)
             }
             (ColumnData::String(data), ColumnData::String(ref mut column_data)) => {
@@ -212,10 +268,16 @@ impl Column {
     pub fn append_datum_ref(&mut self, value: &Datum) -> Result<()> {
         match (&mut self.data, value) {
             (ColumnData::F64(data), Datum::Double(v)) => data[self.to_insert] = *v,
+            (ColumnData::F32(data), Datum::Float(v)) => data[self.to_insert] = *v,
             (ColumnData::I64(data), Datum::Int64(v)) => data[self.to_insert] = *v,
             (ColumnData::I64(data), Datum::Timestamp(v)) => data[self.to_insert] = v.as_i64(),
             (ColumnData::I32(data), Datum::Int32(v)) => data[self.to_insert] = *v,
+            (ColumnData::I16(data), Datum::Int16(v)) => data[self.to_insert] = *v,
+            (ColumnData::I8(data), Datum::Int8(v)) => data[self.to_insert] = *v,
             (ColumnData::U64(data), Datum::UInt64(v)) => data[self.to_insert] = *v,
+            (ColumnData::U32(data), Datum::UInt32(v)) => data[self.to_insert] = *v,
+            (ColumnData::U16(data), Datum::UInt16(v)) => data[self.to_insert] = *v,
+            (ColumnData::U8(data), Datum::UInt8(v)) => data[self.to_insert] = *v,
             (ColumnData::String(data), Datum::String(v)) => data[self.to_insert] = v.to_string(),
             (ColumnData::StringBytes(data), Datum::String(v)) => {
                 data[self.to_insert] = StringBytes::from(v.as_str())
@@ -241,13 +303,19 @@ impl Column {
     pub fn get_datum(&self, idx: usize) -> Datum {
         match self.data {
             ColumnData::F64(ref data) => Datum::Double(data[idx]),
+            ColumnData::F32(ref data) => Datum::Float(data[idx]),
             ColumnData::I64(ref data) => match self.datum_kind {
                 DatumKind::Timestamp => Datum::Timestamp(Timestamp::from(data[idx])),
                 DatumKind::Int64 => Datum::Int64(data[idx]),
                 _ => unreachable!(),
             },
             ColumnData::I32(ref data) => Datum::Int32(data[idx]),
+            ColumnData::I16(ref data) => Datum::Int16(data[idx]),
+            ColumnData::I8(ref data) => Datum::Int8(data[idx]),
             ColumnData::U64(ref data) => Datum::UInt64(data[idx]),
+            ColumnData::U32(ref data) => Datum::UInt32(data[idx]),
+            ColumnData::U16(ref data) => Datum::UInt16(data[idx]),
+            ColumnData::U8(ref data) => Datum::UInt8(data[idx]),
             ColumnData::String(ref data) => Datum::String(data[idx].clone().into()),
             ColumnData::StringBytes(ref data) => Datum::String(data[idx].clone()),
             ColumnData::Varbinary(ref data) => Datum::Varbinary(Bytes::from(data[idx].clone())),
@@ -258,9 +326,15 @@ impl Column {
     pub fn get_value(&self, idx: usize) -> value::Value {
         match self.data {
             ColumnData::F64(ref data) => value::Value::Float64Value(data[idx]),
+            ColumnData::F32(ref data) => value::Value::Float32Value(data[idx]),
             ColumnData::I64(ref data) => value::Value::Int64Value(data[idx]),
             ColumnData::I32(ref data) => value::Value::Int32Value(data[idx]),
+            ColumnData::I16(ref data) => value::Value::Int16Value(data[idx] as i32),
+            ColumnData::I8(ref data) => value::Value::Int8Value(data[idx] as i32),
             ColumnData::U64(ref data) => value::Value::Uint64Value(data[idx]),
+            ColumnData::U32(ref data) => value::Value::Uint32Value(data[idx]),
+            ColumnData::U16(ref data) => value::Value::Uint16Value(data[idx] as u32),
+            ColumnData::U8(ref data) => value::Value::Uint8Value(data[idx] as u32),
             ColumnData::String(ref data) => value::Value::StringValue(data[idx].clone().into()),
             ColumnData::StringBytes(ref data) => value::Value::StringValue(data[idx].to_string()),
             ColumnData::Varbinary(ref data) => value::Value::VarbinaryValue(data[idx].clone()),
@@ -271,9 +345,15 @@ impl Column {
     pub fn get_bytes(&self, idx: usize, buf: &mut BytesMut) {
         match self.data {
             ColumnData::F64(ref data) => buf.put_slice(data[idx].to_le_bytes().as_slice()),
+            ColumnData::F32(ref data) => buf.put_slice(data[idx].to_le_bytes().as_slice()),
             ColumnData::I64(ref data) => buf.put_slice(data[idx].to_le_bytes().as_slice()),
             ColumnData::I32(ref data) => buf.put_slice(data[idx].to_le_bytes().as_slice()),
+            ColumnData::I16(ref data) => buf.put_slice(data[idx].to_le_bytes().as_slice()),
+            ColumnData::I8(ref data) => buf.put_slice(data[idx].to_le_bytes().as_slice()),
             ColumnData::U64(ref data) => buf.put_slice(data[idx].to_le_bytes().as_slice()),
+            ColumnData::U32(ref data) => buf.put_slice(data[idx].to_le_bytes().as_slice()),
+            ColumnData::U16(ref data) => buf.put_slice(data[idx].to_le_bytes().as_slice()),
+            ColumnData::U8(ref data) => buf.put_slice(data[idx].to_le_bytes().as_slice()),
             ColumnData::String(ref data) => buf.put_slice(data[idx].as_bytes()),
             ColumnData::StringBytes(ref data) => buf.put_slice(data[idx].as_bytes()),
             ColumnData::Varbinary(ref data) => buf.put_slice(data[idx].as_slice()),
@@ -311,13 +391,31 @@ impl Column {
             ColumnData::F64(data) => {
                 data.resize(len, 0.);
             }
+            ColumnData::F32(data) => {
+                data.resize(len, 0.);
+            }
             ColumnData::I64(data) => {
                 data.resize(len, 0);
             }
             ColumnData::I32(data) => {
                 data.resize(len, 0);
             }
+            ColumnData::I16(data) => {
+                data.resize(len, 0);
+            }
+            ColumnData::I8(data) => {
+                data.resize(len, 0);
+            }
             ColumnData::U64(data) => {
+                data.resize(len, 0);
+            }
+            ColumnData::U32(data) => {
+                data.resize(len, 0);
+            }
+            ColumnData::U16(data) => {
+                data.resize(len, 0);
+            }
+            ColumnData::U8(data) => {
                 data.resize(len, 0);
             }
             ColumnData::String(data) => {
@@ -349,9 +447,15 @@ impl Column {
     pub fn size(&self) -> usize {
         let data_size = match &self.data {
             ColumnData::F64(v) => mem::size_of::<f64>() * v.capacity(),
+            ColumnData::F32(v) => mem::size_of::<f32>() * v.capacity(),
             ColumnData::I64(v) => mem::size_of::<i64>() * v.capacity(),
             ColumnData::I32(v) => mem::size_of::<i32>() * v.capacity(),
+            ColumnData::I16(v) => mem::size_of::<i16>() * v.capacity(),
+            ColumnData::I8(v) => mem::size_of::<i8>() * v.capacity(),
             ColumnData::U64(v) => mem::size_of::<u64>() * v.capacity(),
+            ColumnData::U32(v) => mem::size_of::<u32>() * v.capacity(),
+            ColumnData::U16(v) => mem::size_of::<u16>() * v.capacity(),
+            ColumnData::U8(v) => mem::size_of::<u8>() * v.capacity(),
             ColumnData::Bool(v) => v.byte_len(),
             ColumnData::String(v) => {
                 v.iter().map(|s| s.len()).sum::<usize>()
@@ -374,9 +478,15 @@ impl Column {
     pub fn size_data(&self) -> usize {
         match &self.data {
             ColumnData::F64(_) => mem::size_of::<f64>() * self.len(),
+            ColumnData::F32(_) => mem::size_of::<f32>() * self.len(),
             ColumnData::I64(_) => mem::size_of::<i64>() * self.len(),
             ColumnData::I32(_) => mem::size_of::<i32>() * self.len(),
+            ColumnData::I16(_) => mem::size_of::<i16>() * self.len(),
+            ColumnData::I8(_) => mem::size_of::<i8>() * self.len(),
             ColumnData::U64(_) => mem::size_of::<u64>() * self.len(),
+            ColumnData::U32(_) => mem::size_of::<u32>() * self.len(),
+            ColumnData::U16(_) => mem::size_of::<u16>() * self.len(),
+            ColumnData::U8(_) => mem::size_of::<u8>() * self.len(),
             ColumnData::Bool(_) => mem::size_of::<bool>() * self.len(),
             ColumnData::String(v) => v.iter().map(|s| s.len()).sum(),
             ColumnData::StringBytes(v) => v.iter().map(|s| s.len()).sum(),
@@ -398,6 +508,15 @@ impl Column {
                     .build()
                     .context(CreatingArrowArray)?;
                 Arc::new(Float64Array::from(data))
+            }
+            ColumnData::F32(data) => {
+                let data = ArrayDataBuilder::new(DataType::Float32)
+                    .len(data.len())
+                    .add_buffer(data.iter().cloned().collect())
+                    .nulls(nulls)
+                    .build()
+                    .context(CreatingArrowArray)?;
+                Arc::new(Float32Array::from(data))
             }
             ColumnData::I64(data) => match self.datum_kind {
                 DatumKind::Timestamp => {
@@ -430,6 +549,24 @@ impl Column {
                     .context(CreatingArrowArray)?;
                 Arc::new(Int32Array::from(data))
             }
+            ColumnData::I16(data) => {
+                let data = ArrayDataBuilder::new(DataType::Int16)
+                    .len(data.len())
+                    .add_buffer(data.iter().cloned().collect())
+                    .nulls(nulls)
+                    .build()
+                    .context(CreatingArrowArray)?;
+                Arc::new(Int16Array::from(data))
+            }
+            ColumnData::I8(data) => {
+                let data = ArrayDataBuilder::new(DataType::Int8)
+                    .len(data.len())
+                    .add_buffer(data.iter().cloned().collect())
+                    .nulls(nulls)
+                    .build()
+                    .context(CreatingArrowArray)?;
+                Arc::new(Int8Array::from(data))
+            }
             ColumnData::U64(data) => {
                 let data = ArrayDataBuilder::new(DataType::UInt64)
                     .len(data.len())
@@ -438,6 +575,33 @@ impl Column {
                     .build()
                     .context(CreatingArrowArray)?;
                 Arc::new(UInt64Array::from(data))
+            }
+            ColumnData::U32(data) => {
+                let data = ArrayDataBuilder::new(DataType::UInt32)
+                    .len(data.len())
+                    .add_buffer(data.iter().cloned().collect())
+                    .nulls(nulls)
+                    .build()
+                    .context(CreatingArrowArray)?;
+                Arc::new(UInt32Array::from(data))
+            }
+            ColumnData::U16(data) => {
+                let data = ArrayDataBuilder::new(DataType::UInt16)
+                    .len(data.len())
+                    .add_buffer(data.iter().cloned().collect())
+                    .nulls(nulls)
+                    .build()
+                    .context(CreatingArrowArray)?;
+                Arc::new(UInt16Array::from(data))
+            }
+            ColumnData::U8(data) => {
+                let data = ArrayDataBuilder::new(DataType::UInt8)
+                    .len(data.len())
+                    .add_buffer(data.iter().cloned().collect())
+                    .nulls(nulls)
+                    .build()
+                    .context(CreatingArrowArray)?;
+                Arc::new(UInt8Array::from(data))
             }
             ColumnData::String(data) => {
                 let data =
