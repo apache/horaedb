b6a0563226035bfa704a69fed685d030dea6f2ef
jiacai2050 <dev@liujiacai.net>
2023-08-02 10:36:55 +0800

    fix CR

--- components/table_kv/src/metrics.rs
+++ components/table_kv/src/metrics.rs
@@ -4,12 +4,12 @@ use lazy_static::lazy_static;
 use prometheus::{exponential_buckets, register_histogram_vec, HistogramVec};
 
 lazy_static! {
-    // Buckets: 0.001, .., 0.01 * 2^10
+    // Buckets: 0.001, .., 0.001 * 2^15 = 32.7s
     pub static ref OBKV_OP_DURATION_HISTOGRAM: HistogramVec = register_histogram_vec!(
         "obkv_op_duration",
         "Histogram for duration of different obkv operations",
         &["type"],
-        exponential_buckets(0.001, 2.0, 10).unwrap()
-        )
+        exponential_buckets(0.001, 2.0, 15).unwrap()
+    )
     .unwrap();
 }
