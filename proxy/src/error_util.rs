3a6df97a19e68b35977e02c41de7db7c8b5bd886
tanruixiang <819464715@qq.com>
2023-08-05 11:16:08 +0800

    fmt

--- proxy/src/error_util.rs
+++ proxy/src/error_util.rs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 // Our backtrace is defined like this
 // #[snafu(display("Time range is not found.\nBacktrace\n:{}", backtrace))]
 //
