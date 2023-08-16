eb4ce1ed198cb67b7a9bc1ea1a63b8f1aa9ccb61
kamille <caoruiqiu.crq@antgroup.com>
2023-08-16 11:43:25 +0800

    remove some useless things.

--- df_engine_extensions/src/dist_sql_query/partitioned_table_scan.rs
+++ df_engine_extensions/src/dist_sql_query/partitioned_table_scan.rs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Copyright 2023 CeresDB Project Authors. Licensed under Apache-2.0.
-
 use std::{any::Any, fmt, sync::Arc};
 
 use arrow::datatypes::SchemaRef;
