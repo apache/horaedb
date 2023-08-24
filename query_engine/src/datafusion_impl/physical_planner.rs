3090d8c38ac367bf57bb68b4a5c130ccaa28637e
kamille <caoruiqiu.crq@antgroup.com>
2023-08-24 22:22:24 +0800

    support to generate typed physical plans.

--- query_engine/src/datafusion_impl/physical_planner.rs
+++ query_engine/src/datafusion_impl/physical_planner.rs
@@ -39,10 +39,25 @@ impl DatafusionPhysicalPlannerImpl {
     pub fn new(df_ctx_builder: Arc<DfContextBuilder>) -> Self {
         Self { df_ctx_builder }
     }
+
+    fn has_partitioned_table(logical_plan: &QueryPlan) -> bool {
+        let mut has_partitioned_table = false;
+        let _ = logical_plan
+            .tables
+            .visit(|_, table| -> std::result::Result<(), ()> {
+                if table.partition_info().is_some() {
+                    has_partitioned_table = true;
+                }
+                Ok(())
+            });
+
+        has_partitioned_table
+    }
 }
 
 #[async_trait]
 impl PhysicalPlanner for DatafusionPhysicalPlannerImpl {
+    // TODO: we should modify `QueryPlan` to support create remote plan here.
     async fn plan(&self, ctx: &Context, logical_plan: QueryPlan) -> Result<PhysicalPlanPtr> {
         // Register catalogs to datafusion execution context.
         let catalogs = CatalogProviderAdapter::new_adapters(logical_plan.tables.clone());
@@ -62,7 +77,16 @@ impl PhysicalPlanner for DatafusionPhysicalPlannerImpl {
             .await
             .box_err()
             .context(PhysicalPlannerWithCause { msg: None })?;
-        let physical_plan = DataFusionPhysicalPlanAdapter::new(TypedPlan::Normal(exec_plan));
+
+        // Decide if partitioned table exists.
+        let has_partitioned_table =
+            DatafusionPhysicalPlannerImpl::has_partitioned_table(&logical_plan);
+        let typed_plan = if has_partitioned_table {
+            TypedPlan::Partitioned(exec_plan)
+        } else {
+            TypedPlan::Normal(exec_plan)
+        };
+        let physical_plan = DataFusionPhysicalPlanAdapter::new(typed_plan);
 
         Ok(Box::new(physical_plan))
     }
