6f90a7ce7cd63cc4d23133328b01292a39e1e8f3
kamille <34352236+Rachelint@users.noreply.github.com>
2023-09-08 16:52:28 +0800

    feat: refactor `Resolver` in dist sql query (#1186)
    ## Rationale
    Part of #1112 
    The old resolver has some problems, need to refactor before adding it
    into the main query process.
    
    ## Detailed Changes
    + Refactor `Resolver` in dist sql query.
    
    ## Test Plan
    Test by richer unit tests.

--- df_engine_extensions/src/dist_sql_query/test_util.rs
+++ df_engine_extensions/src/dist_sql_query/test_util.rs
@@ -12,30 +12,47 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::sync::Arc;
+use std::{
+    pin::Pin,
+    sync::Arc,
+    task::{Context, Poll},
+};
 
+use arrow::{
+    datatypes::{Schema, SchemaRef},
+    record_batch::RecordBatch,
+};
+use async_trait::async_trait;
 use catalog::{manager::ManagerRef, test_util::MockCatalogManagerBuilder};
 use common_types::{projected_schema::ProjectedSchema, tests::build_schema_for_cpu};
 use datafusion::{
+    error::{DataFusionError, Result as DfResult},
+    execution::{runtime_env::RuntimeEnv, FunctionRegistry, TaskContext},
     logical_expr::{expr_fn, Literal, Operator},
     physical_plan::{
         expressions::{binary, col, lit},
         filter::FilterExec,
         projection::ProjectionExec,
-        ExecutionPlan, PhysicalExpr,
+        DisplayAs, EmptyRecordBatchStream, ExecutionPlan, PhysicalExpr, RecordBatchStream,
+        SendableRecordBatchStream,
     },
     scalar::ScalarValue,
 };
+use futures::{future::BoxFuture, Stream};
 use table_engine::{
     memory::MemoryTable,
     predicate::PredicateBuilder,
     remote::model::TableIdentifier,
-    table::{ReadOptions, ReadRequest, TableId},
+    table::{ReadOptions, ReadRequest, TableId, TableRef},
     ANALYTIC_ENGINE_TYPE,
 };
 use trace_metric::MetricsCollector;
 
-use crate::dist_sql_query::physical_plan::{UnresolvedPartitionedScan, UnresolvedSubTableScan};
+use crate::dist_sql_query::{
+    physical_plan::{PartitionedScanStream, UnresolvedPartitionedScan, UnresolvedSubTableScan},
+    resolver::Resolver,
+    EncodedPlan, ExecutableScanBuilder, RemotePhysicalPlanExecutor,
+};
 
 // Test context
 pub struct TestContext {
@@ -164,6 +181,17 @@ impl TestContext {
         }
     }
 
+    // Return resolver
+    pub fn resolver(&self) -> Resolver {
+        Resolver::new(
+            Arc::new(MockRemotePhysicalPlanExecutor),
+            self.catalog_manager.clone(),
+            Box::new(MockScanBuilder),
+            Arc::new(RuntimeEnv::default()),
+            Arc::new(MockFunctionRegistry),
+        )
+    }
+
     // Return test catalog manager
     pub fn catalog_manager(&self) -> ManagerRef {
         self.catalog_manager.clone()
@@ -201,7 +229,217 @@ impl TestContext {
         Arc::new(ProjectionExec::try_new(self.physical_projection.clone(), filter).unwrap())
     }
 
+    // Plan that should not be processed by resolver.
+    pub fn build_unprocessed_plan(&self) -> Arc<dyn ExecutionPlan> {
+        let mock_scan = Arc::new(MockScan {
+            request: self.request.clone(),
+        });
+
+        Arc::new(ProjectionExec::try_new(self.physical_projection.clone(), mock_scan).unwrap())
+    }
+
     pub fn read_request(&self) -> ReadRequest {
         self.request.clone()
+    }
+}
+
+// Mock function registry
+struct MockFunctionRegistry;
+
+impl FunctionRegistry for MockFunctionRegistry {
+    fn udfs(&self) -> std::collections::HashSet<String> {
+        unimplemented!()
+    }
+
+    fn udf(&self, _name: &str) -> DfResult<Arc<datafusion::logical_expr::ScalarUDF>> {
+        unimplemented!()
+    }
+
+    fn udaf(&self, _name: &str) -> DfResult<Arc<datafusion::logical_expr::AggregateUDF>> {
+        unimplemented!()
+    }
+
+    fn udwf(&self, _name: &str) -> DfResult<Arc<datafusion::logical_expr::WindowUDF>> {
+        unimplemented!()
+    }
+}
+
+// Mock scan and its builder
+#[derive(Debug)]
+struct MockScanBuilder;
+
+#[async_trait]
+impl ExecutableScanBuilder for MockScanBuilder {
+    async fn build(
+        &self,
+        _table: TableRef,
+        read_request: ReadRequest,
+    ) -> datafusion::error::Result<Arc<dyn ExecutionPlan>> {
+        Ok(Arc::new(MockScan {
+            request: read_request,
+        }))
+    }
+}
+
+#[derive(Debug)]
+struct MockScan {
+    request: ReadRequest,
+}
+
+impl ExecutionPlan for MockScan {
+    fn as_any(&self) -> &dyn std::any::Any {
+        self
+    }
+
+    fn schema(&self) -> arrow::datatypes::SchemaRef {
+        self.request.projected_schema.to_projected_arrow_schema()
+    }
+
+    fn output_partitioning(&self) -> datafusion::physical_plan::Partitioning {
+        datafusion::physical_plan::Partitioning::UnknownPartitioning(
+            self.request.opts.read_parallelism,
+        )
+    }
+
+    fn output_ordering(&self) -> Option<&[datafusion::physical_expr::PhysicalSortExpr]> {
+        None
+    }
+
+    fn children(&self) -> Vec<Arc<dyn ExecutionPlan>> {
+        vec![]
+    }
+
+    fn with_new_children(
+        self: Arc<Self>,
+        _children: Vec<Arc<dyn ExecutionPlan>>,
+    ) -> datafusion::error::Result<Arc<dyn ExecutionPlan>> {
+        unimplemented!()
+    }
+
+    fn execute(
+        &self,
+        _partition: usize,
+        _context: Arc<datafusion::execution::TaskContext>,
+    ) -> datafusion::error::Result<datafusion::physical_plan::SendableRecordBatchStream> {
+        unimplemented!()
+    }
+
+    fn statistics(&self) -> datafusion::physical_plan::Statistics {
+        unimplemented!()
+    }
+}
+
+impl DisplayAs for MockScan {
+    fn fmt_as(
+        &self,
+        _t: datafusion::physical_plan::DisplayFormatType,
+        f: &mut std::fmt::Formatter,
+    ) -> std::fmt::Result {
+        write!(f, "MockScan")
+    }
+}
+
+// Mock remote executor
+#[derive(Debug, Clone)]
+struct MockRemotePhysicalPlanExecutor;
+
+impl RemotePhysicalPlanExecutor for MockRemotePhysicalPlanExecutor {
+    fn execute(
+        &self,
+        _table: TableIdentifier,
+        _task_context: &TaskContext,
+        _encoded_plan: EncodedPlan,
+    ) -> DfResult<BoxFuture<'static, DfResult<SendableRecordBatchStream>>> {
+        unimplemented!()
+    }
+}
+
+/// Used in [PartitionedScanStream]'s testing
+pub struct MockPartitionedScanStreamBuilder {
+    schema: SchemaRef,
+    case: PartitionedScanStreamCase,
+}
+
+#[derive(Clone, Copy)]
+pub enum PartitionedScanStreamCase {
+    InitializeFailed,
+    PollFailed,
+    Success,
+}
+
+impl MockPartitionedScanStreamBuilder {
+    pub(crate) fn new(case: PartitionedScanStreamCase) -> Self {
+        let schema = Arc::new(Schema::empty());
+        Self { schema, case }
+    }
+
+    pub(crate) fn build(&self) -> PartitionedScanStream {
+        let stream_future: BoxFuture<'static, DfResult<SendableRecordBatchStream>> = match self.case
+        {
+            PartitionedScanStreamCase::InitializeFailed => {
+                Box::pin(
+                    async move { Err(DataFusionError::Internal("failed to init".to_string())) },
+                )
+            }
+            PartitionedScanStreamCase::PollFailed => {
+                let error_stream = self.build_error_record_stream();
+                Box::pin(async move { Ok(error_stream) })
+            }
+            PartitionedScanStreamCase::Success => {
+                let success_stream = self.build_success_record_stream();
+                Box::pin(async move { Ok(success_stream) })
+            }
+        };
+
+        PartitionedScanStream::new(stream_future, self.schema.clone())
+    }
+
+    #[inline]
+    fn build_error_record_stream(&self) -> SendableRecordBatchStream {
+        Box::pin(ErrorRecordBatchStream::new(self.schema.clone()))
+    }
+
+    #[inline]
+    fn build_success_record_stream(&self) -> SendableRecordBatchStream {
+        Box::pin(EmptyRecordBatchStream::new(self.schema.clone()))
+    }
+}
+
+/// ErrorRecordBatchStream which will produce error results
+pub struct ErrorRecordBatchStream {
+    /// Schema wrapped by Arc
+    schema: SchemaRef,
+
+    /// Mark the stream is terminated.
+    done: bool,
+}
+
+impl ErrorRecordBatchStream {
+    pub fn new(schema: SchemaRef) -> Self {
+        Self {
+            schema,
+            done: false,
+        }
+    }
+}
+
+impl RecordBatchStream for ErrorRecordBatchStream {
+    fn schema(&self) -> SchemaRef {
+        self.schema.clone()
+    }
+}
+
+impl Stream for ErrorRecordBatchStream {
+    type Item = DfResult<RecordBatch>;
+
+    fn poll_next(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        if self.done {
+            return Poll::Ready(None);
+        }
+
+        self.get_mut().done = true;
+        Poll::Ready(Some(Err(DataFusionError::Internal(
+            "failed to poll".to_string(),
+        ))))
     }
 }
