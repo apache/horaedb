25d12d670f5dde71e1cbcbdaa3b8e303329c0e51
Jiacai Liu <dev@liujiacai.net>
2023-09-14 17:34:37 +0800

    Update analytic_engine/src/memtable/skiplist/mod.rs
    Co-authored-by: WEI Xikai <ShiKaiWi@users.noreply.github.com>

--- analytic_engine/src/memtable/skiplist/mod.rs
+++ analytic_engine/src/memtable/skiplist/mod.rs
@@ -156,7 +156,7 @@ impl<A: Arena<Stats = BasicStats> + Clone + Sync + Send + 'static> MemTable
             .load(atomic::Ordering::Relaxed);
 
         let arena_block_size = self.skiplist.arena_block_size();
-        encoded_size / arena_block_size * arena_block_size + arena_block_size
+        (encoded_size  + arena_block_size - 1)/ arena_block_size * arena_block_size
     }
 
     fn set_last_sequence(&self, sequence: SequenceNumber) -> Result<()> {
