dc5be188bf2bc9ad18074a0f78495c845ac59770
kamille <caoruiqiu.crq@antgroup.com>
2023-09-29 15:31:02 +0800

    fix clippy.

--- query_engine/src/datafusion_impl/physical_plan.rs
+++ query_engine/src/datafusion_impl/physical_plan.rs
@@ -120,7 +120,7 @@ impl PhysicalPlan for DataFusionPhysicalPlanAdapter {
         };
 
         info!(
-            "DatafusionExecutorImpl get the executable plan, request_id:{}, physical_plan: {}",
+            "DatafusionExecutorImpl get the executable plan, request_id:{}, physical_plan:{}",
             df_task_ctx.request_id,
             displayable(executable.as_ref()).indent(true).to_string()
         );
