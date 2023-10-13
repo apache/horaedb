bfc1a5d0bc45bc4c555db888eb138de4f97a9e69
tanruixiang <tanruixiang0104@gmail.com>
2023-10-13 17:48:14 +0800

    clippy

--- proxy/src/error.rs
+++ proxy/src/error.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::io;
+
 
 use ceresdbproto::common::ResponseHeader;
 use generic_error::GenericError;
