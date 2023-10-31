5375610a631ecf30c5e876a28a97c558931046ca
tison <wander4096@gmail.com>
2023-10-31 19:13:30 +0800

    use a more idiom way
    Signed-off-by: tison <wander4096@gmail.com>

--- integration_tests/build_meta.sh
+++ integration_tests/build_meta.sh
@@ -7,8 +7,7 @@ set -o pipefail
 BIN_PATH=${BIN_PATH:-""}
 if [[ -z "${BIN_PATH}" ]]; then
   echo "Fetch and install ceresmeta-server..."
-  COMMIT=$(gh api -H "Accept: application/vnd.github.VERSION.sha" repos/CeresDB/ceresmeta/commits/main)
-  go install github.com/CeresDB/ceresmeta/cmd/ceresmeta-server@${COMMIT}
+  go install github.com/CeresDB/ceresmeta/cmd/ceresmeta-server@HEAD
   BIN_PATH="$(go env GOPATH)/bin/ceresmeta-server"
 fi
 
