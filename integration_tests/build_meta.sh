325ed04cf60635554a4d0e00a2651e64c4f7af23
CooooolFrog <zuliangwanghust@gmail.com>
2023-11-09 14:17:51 +0800

    fix build_meta script

--- integration_tests/build_meta.sh
+++ integration_tests/build_meta.sh
@@ -7,7 +7,7 @@ set -o pipefail
 BIN_PATH=${BIN_PATH:-""}
 
 echo "Fetch and install ceresmeta-server..."
-go install -a github.com/CeresDB/ceresmeta/cmd/ceresmeta-server@HEAD
+go install -a github.com/CeresDB/ceresmeta/cmd/ceresmeta-server@main
 BIN_PATH="$(go env GOPATH)/bin/ceresmeta-server"
 
 TARGET=$(pwd)/ceresmeta
