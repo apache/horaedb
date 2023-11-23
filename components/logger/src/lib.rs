216bb1d30b2a4eff3dfd49c0508c720a0e6e2681
jiacai2050 <dev@liujiacai.net>
2023-11-23 16:10:56 +0800

    feat: slow log include remote query

--- components/logger/src/lib.rs
+++ components/logger/src/lib.rs
@@ -476,13 +476,14 @@ pub struct SlowTimer<'a> {
 impl<'a> Drop for SlowTimer<'a> {
     fn drop(&mut self) {
         let cost = self.elapsed();
-        maybe_slow_query!(
-            self,
-            "RequestId:{}, elapsed:{:?}, sql:{}",
-            self.request_id,
-            cost,
-            self.sql,
-        );
+        if cost > self.slow_threshold {
+            slow_query!(
+                "RequestId:{}, elapsed:{:?}, sql:{}",
+                self.request_id,
+                cost,
+                self.sql,
+            );
+        }
     }
 }
 
@@ -498,10 +499,6 @@ impl<'a> SlowTimer<'a> {
 
     pub fn elapsed(&self) -> Duration {
         self.start_time.elapsed()
-    }
-
-    pub fn is_slow(&self) -> bool {
-        self.elapsed() >= self.slow_threshold
     }
 
     pub fn start_time(&self) -> Instant {
@@ -565,11 +562,9 @@ macro_rules! trace {
 }
 
 #[macro_export(local_inner_macros)]
-macro_rules! maybe_slow_query {
-    ($t:expr, $($args:tt)*) => {{
-        if $t.is_slow() {
-            info!(target: $crate::SLOW_QUERY_TAG, $($args)*);
-        }
+macro_rules! slow_query {
+    ($($args:tt)*) => {{
+        info!(target: $crate::SLOW_QUERY_TAG, $($args)*);
     }}
 }
 
--- query_engine/src/datafusion_impl/task_context.rs
+++ query_engine/src/datafusion_impl/task_context.rs
@@ -24,7 +24,7 @@ use common_types::{request_id::RequestId, schema::RecordSchema};
 use datafusion::{
     error::{DataFusionError, Result as DfResult},
     execution::{runtime_env::RuntimeEnv, FunctionRegistry, TaskContext},
-    physical_plan::{ExecutionPlan, SendableRecordBatchStream},
+    physical_plan::{display::DisplayableExecutionPlan, ExecutionPlan, SendableRecordBatchStream},
 };
 use datafusion_proto::{
     bytes::physical_plan_to_bytes_with_extension_codec,
@@ -191,11 +191,14 @@ impl RemotePhysicalPlanExecutor for RemotePhysicalPlanExecutorImpl {
         let default_catalog = ceresdb_options.default_catalog.clone();
         let default_schema = ceresdb_options.default_schema.clone();
 
+        let display_plan = DisplayableExecutionPlan::new(plan.as_ref());
+        let sql = display_plan.indent(true).to_string();
         let exec_ctx = ExecContext {
             request_id,
             deadline,
             default_catalog,
             default_schema,
+            sql,
         };
 
         // Encode plan and schema
