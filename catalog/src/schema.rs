ed4719d5d40a400cbcd7fd7fcbb7a66e48fa766d
kamille <caoruiqiu.crq@antgroup.com>
2023-12-22 16:26:43 +0800

    fix integration test.

--- catalog/src/schema.rs
+++ catalog/src/schema.rs
@@ -1,3 +1,17 @@
+// Copyright 2023 The CeresDB Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Copyright 2023 The HoraeDB Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -26,6 +40,7 @@ use table_engine::{
     table::{SchemaId, TableId, TableRef},
 };
 
+// FIXME: `CreateExistTable` can lead to `segmentation fault` if including backtrace.
 #[derive(Debug, Snafu)]
 #[snafu(visibility(pub))]
 pub enum Error {
@@ -119,11 +134,9 @@ pub enum Error {
     CloseTableWithCause { source: GenericError },
 
     #[snafu(display(
-        "Failed to create table, table already exists, table:{}.\nBacktrace:\n{}",
-        table,
-        backtrace
+        "Failed to create table, table already exists, table:{table}."
     ))]
-    CreateExistTable { table: String, backtrace: Backtrace },
+    CreateExistTable { table: String },
 
     #[snafu(display(
         "Failed to create table, cannot persist meta, table:{}, err:{}",
