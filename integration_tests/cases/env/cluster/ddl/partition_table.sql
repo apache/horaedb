16c9d9830a237fecd8d179fae0807cbb86bc4cd1
jiacai2050 <dev@liujiacai.net>
2024-01-04 12:40:39 +0800

    fix ci

--- integration_tests/cases/env/cluster/ddl/partition_table.sql
+++ integration_tests/cases/env/cluster/ddl/partition_table.sql
@@ -46,6 +46,7 @@ EXPLAIN ANALYZE SELECT * from partition_table_t where name in ("ceresdb0", "cere
 
 ALTER TABLE partition_table_t ADD COLUMN (b string);
 
+-- SQLNESS REPLACE endpoint:(.*?), endpoint:xx,
 INSERT INTO partition_table_t (t, id, name, value) VALUES (1651737067000, 10, "ceresdb0", 100);
 
 ALTER TABLE partition_table_t MODIFY SETTING enable_ttl='true';
