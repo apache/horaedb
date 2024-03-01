3ad9453c422f83f981c8f686b1c7f2009f62a593
chunshao.rcs <chunshao@apache.org>
2024-03-01 16:43:36 +0800

    add ASF license header

--- horaemeta/server/coordinator/scheduler/nodepicker/hash/consistent_uniform.go
+++ horaemeta/server/coordinator/scheduler/nodepicker/hash/consistent_uniform.go
@@ -21,6 +21,25 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 // This file is copied from:
 // https://github.com/buraksezer/consistent/blob/4516339c49db00f725fa89d0e3e7e970e4039af0/consistent.go
 // Package hash provides a consistent hashing function with bounded loads.
