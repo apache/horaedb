1533048cb66df12f54647406fc12f81b7e24d06e
neochen <neochen428@gmail.com>
2025-02-08 23:49:29 -0600

    add metrics cache

--- src/metric_engine/src/types.rs
+++ src/metric_engine/src/types.rs
@@ -15,8 +15,72 @@
 // specific language governing permissions and limitations
 // under the License.
 
-pub struct MetricId(u64);
+use std::{
+    cmp::Reverse,
+    collections::{BinaryHeap, HashMap},
+    time::{Duration, SystemTime},
+};
+
+use chrono::{DateTime, Datelike, TimeZone, Utc};
+use tokio::sync::RwLock;
+pub struct MetricId(pub u64);
 pub struct SeriesId(u64);
+
+pub const METRIC_NAME: &str = "__name__";
+pub const DEFAULT_FIELD_NAME: &str = "value";
+pub const DEFAULT_FIELD_TYPE: u8 = 0;
+
+pub type MetricName = Vec<u8>;
+pub type FieldName = Vec<u8>;
+pub type FieldType = u8;
+pub type TimeStamp = i64;
+
+pub struct Task {
+    pub create_time: Duration,
+    pub timestamp: TimeStamp,
+    pub data: TaskData,
+}
+
+pub enum TaskData {
+    Metric(MetricName, FieldName, FieldType),
+}
+
+impl Task {
+    pub fn metric_task(
+        timestamp: TimeStamp,
+        metric_name: MetricName,
+        field_name: FieldName,
+        field_type: FieldType,
+    ) -> Self {
+        Self {
+            create_time: SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .unwrap(),
+            timestamp,
+            data: TaskData::Metric(metric_name, field_name, field_type),
+        }
+    }
+}
+
+#[derive(Copy, Clone, Eq, PartialEq, Hash, Debug)]
+pub struct SegmentTimeStamp(i64);
+
+impl SegmentTimeStamp {
+    pub fn date(timestamp: i64) -> Self {
+        let dt = DateTime::from_timestamp_millis(timestamp).unwrap();
+        let start_of_day = Utc
+            .with_ymd_and_hms(dt.year(), dt.month(), dt.day(), 0, 0, 0)
+            .unwrap();
+
+        Self(start_of_day.timestamp())
+    }
+
+    pub fn day_diff(lhs: i64, rhs: i64) -> i32 {
+        let lhs = DateTime::from_timestamp_millis(lhs).unwrap();
+        let rhs = DateTime::from_timestamp_millis(rhs).unwrap();
+        lhs.num_days_from_ce() - rhs.num_days_from_ce()
+    }
+}
 
 pub struct Label {
     pub name: Vec<u8>,
@@ -28,7 +92,7 @@ pub struct Label {
 pub struct Sample {
     pub name: Vec<u8>,
     pub lables: Vec<Label>,
-    pub timestamp: i64,
+    pub timestamp: i64, // millisecond
     pub value: f64,
     /// hash of name
     pub name_id: Option<MetricId>,
@@ -38,4 +102,81 @@ pub struct Sample {
 
 pub fn hash(buf: &[u8]) -> u64 {
     seahash::hash(buf)
+}
+
+pub struct SectionedHashMap<V> {
+    sections: Vec<RwLock<HashMap<usize, V>>>,
+    section_count: usize,
+    min_keys: RwLock<BinaryHeap<Reverse<usize>>>,
+}
+
+impl<V> SectionedHashMap<V> {
+    pub fn new(section_count: usize) -> Self {
+        assert!(section_count > 0, "Section count must be greater than 0");
+
+        let mut sections = Vec::with_capacity(section_count);
+        for _ in 0..section_count {
+            sections.push(RwLock::new(HashMap::new()));
+        }
+
+        SectionedHashMap {
+            sections,
+            section_count,
+            min_keys: RwLock::new(BinaryHeap::new()),
+        }
+    }
+
+    #[inline]
+    fn get_section_index(&self, key: usize) -> usize {
+        key % self.section_count
+    }
+
+    #[inline]
+    pub fn get_section_count(&self) -> usize {
+        self.section_count
+    }
+
+    pub async fn insert(&self, key: usize, value: V) -> Option<V>
+    where
+        V: Clone,
+    {
+        let section_idx = self.get_section_index(key);
+        let mut write_guard = self.sections[section_idx].write().await;
+        let (inserted, result) =
+            if let std::collections::hash_map::Entry::Vacant(e) = write_guard.entry(key) {
+                let result = e.insert(value);
+                (true, Some(result.clone()))
+            } else {
+                (false, write_guard.get(&key).cloned())
+            };
+        drop(write_guard);
+        if inserted {
+            self.min_keys.write().await.push(Reverse(key));
+        }
+        result
+    }
+
+    pub async fn get(&self, key: usize) -> Option<V>
+    where
+        V: Clone,
+    {
+        let section_idx = self.get_section_index(key);
+        let section = self.sections[section_idx].read().await;
+        section.get(&key).cloned()
+    }
+
+    pub async fn evict_oldest(&self, oldest_day: usize) {
+        let mut min_keys = self.min_keys.write().await;
+        while min_keys.peek().unwrap().0 <= oldest_day {
+            let min_key = {
+                match min_keys.pop() {
+                    Some(Reverse(key)) => key,
+                    None => return,
+                }
+            };
+
+            let section_idx = self.get_section_index(min_key);
+            self.sections[section_idx].write().await.remove(&min_key);
+        }
+    }
 }
